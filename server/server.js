1419c3299d0ad01af6166945c89ea077740fbd2f
Deepak-132006 <deepakn132006@gmail.com>
2025-12-27 19:50:43 +0530

    Finished User Authentication

--- server/server.js
+++ server/server.js
@@ -2,18 +2,20 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import authRoutes from "./routes/auth.js"
 
 dotenv.config();
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/auth", authRoutes)
 
 connectDB();
-
 app.get("/", (req, res) => {
     res.send("Server is running...")
 });
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=> console.log(`Server is running om http://localhost:${PORT}`))
