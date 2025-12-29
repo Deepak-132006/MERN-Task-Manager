0f3faac77957bbdc0eff2716c45175730927b7d6
Deepak-132006 <deepakn132006@gmail.com>
2025-12-29 11:03:31 +0530

    CRUD Done

--- server/routes/task.js
+++ server/routes/task.js
@@ -29,13 +29,81 @@ router.post("/", async (req, res) => {
 });
 
 router.get("/", async (req, res) => {
-    try{
-        const tasks = await Task.find({user: req.user.id})
-        .sort({createdAt: -1})
-        res.status(200).json(tasks)
-    } catch (error) {
-        res.status(500).json({message: "Server Error"})
+  try {
+    const filter = {user: req.user.id}
+
+    if(req.query.status){
+      filter.status = req.query.status;
     }
-})
+    const tasks = await Task.find(filter).sort({
+      createdAt: -1,
+    });
+    res.status(200).json(tasks);
+  } catch (error) {
+    console.error("GET TASKS ERROR:", error);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const taskId = req.params.id;
+    const { title, description, status, dueDate } = req.body;
+
+    const task = await Task.findById(taskId);
+
+    if (!task) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+
+    if (task.user.toString() !== req.user.id) {
+      return res.status(401).json({ message: "Authorization denied" });
+    }
+
+    if (title !== undefined) task.title = title;
+    if (description !== undefined) task.description = description;
+    if (status !== undefined) task.status = status;
+    if (dueDate !== undefined) task.dueDate = dueDate;
+
+    const updatedTask = await task.save();
+
+    res.status(200).json(updatedTask);
+  } catch (error) {
+    console.error("UPDATE TASK ERROR: ", error);
+    res.status(500).json({ message: "Server Error" });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id);
+    if (!task) return res.status(404).json({ message: "Task not found" });
+
+    if (task.user.toString() !== req.user.id)
+      return res.status(401).json({ message: "Authorization Denied" });
+
+    await task.deleteOne();
+    res.json({ message: "Task removed successfully" });
+  } catch (error) {
+    console.error("DELETE TASK ERROR", error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+router.patch("/:id/toggle", async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id);
+    if(!task) return res.status(404).json({message: "Task not found"})
+
+    if(task.user.toString() !== req.user.id) return res.status(401).json({message: "Authorization Denied"})
+
+    task.status = task.status === "pending" ? "completed" : "pending"
+    await task.save()
+    res.json(task)
+  } catch (error){
+    console.error("TOGGLE STATUS ERROR", error);
+    res.status(500).json({message: "Server error"})
+  }
+});
 
 export default router;
