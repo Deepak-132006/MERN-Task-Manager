b4df73e5fccdf07a9ada5b3714590f264df98268
Deepak-132006 <deepakn132006@gmail.com>
2025-12-29 11:10:06 +0530

    Added another route for Get

--- server/routes/task.js
+++ server/routes/task.js
@@ -1,4 +1,4 @@
-import express, { Router } from "express";
+import express from "express";
 import Task from "../models/Task.js";
 import auth from "../middleware/authMiddleware.js";
 
@@ -42,6 +42,20 @@ router.get("/", async (req, res) => {
   } catch (error) {
     console.error("GET TASKS ERROR:", error);
     res.status(500).json({ message: "Server Error" });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const task = await Task.findById(req.params.id);
+    if (!task) return res.status(404).json({ message: "Task not found" });
+    if (task.user.toString() !== req.user.id)
+      return res.status(401).json({ message: "Authorization denied" });
+
+    res.status(200).json(task);
+  } catch (error) {
+    console.error("GET SINGLE TASK ERROR:", error);
+    res.status(500).json({ message: "Server error" });
   }
 });
 
