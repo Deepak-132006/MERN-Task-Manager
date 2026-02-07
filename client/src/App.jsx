d626d65a709ba1e8dcfc8d345d17619903033daa
Deepak-132006 <deepakn132006@gmail.com>
2026-02-07 20:51:05 +0530

    Starting from Scratch

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,15 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Signup from "./pages/Signup";
-import Login from "./pages/Login";
-import { AuthProvider, AuthContext } from "./context/AuthContext";
-import { useContext } from "react";
+import React from 'react'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
 
-
-function ProtectedRoute({children}){
-  const {token} = useContext(AuthContext);
-  return token ? children : <Navigate to="/login"/>;
-}
 
 const App = () => {
   return (
     <div>
-      <AuthProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/signup" element={<Signup/>}/>
-            <Route path="/login" element={<Login/>}/>
-            <Route path="/dashboard" element={<ProtectedRoute><h1>Dashboard</h1></ProtectedRoute>}/>
-          </Routes>
-        </BrowserRouter>
-      </AuthProvider>
+      <Login/>
+      <Signup/>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
