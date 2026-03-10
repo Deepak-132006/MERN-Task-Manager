7770464cbbac6cf5cbe35a3f77b2d7653c97b002
Deepak-132006 <deepakn132006@gmail.com>
2026-03-10 22:42:54 +0530

    Filterbar Completed

---
+++ client/src/components/FilterBar.jsx
@@ -0,0 +1,15 @@
+function FilterBar({ filter, setFilter, onAdd }) {
+  const filters = ["all", "pending", "completed"];
+  return (
+    <div>
+      <div>
+        {filters.map((f) => {
+          <button key={f} onClick={() => setFilter(f)}>
+            {f}
+          </button>;
+        })}
+      </div>
+      <button onClick={onAdd}>Add Task</button>
+    </div>
+  );
+}
--- client/src/components/TaskForm.jsx
+++ client/src/components/TaskForm.jsx
@@ -1,12 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
-const TaskForm = () => {
-  
-  return (
-    <div>
+const TaskForm = ({ task, onSubmit, onClose }) => {
+  const isEdit = !!task;
+  const [form, setForm] = useState({
+    title: "",
+    description: "",
+    dueDate: "",
+  });
+  useEffect(() => {
+    if (isEdit) {
+      setForm({
+        title: task.title || "",
+        description: task.description || "",
+        dueDate: task.dueDate ? task.dueDate.split("T")[0] : "",
+      });
+    }
+  }, [task]);
+  const handleChange = (e) =>
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      if(!form.title.trim()) return alert("Title required")
+        isEdit ? onSubmit(task._id, form) : 
+    }
+  return <div></div>;
+};
 
-    </div>
-  )
-}
-
-export default TaskForm
+export default TaskForm;
