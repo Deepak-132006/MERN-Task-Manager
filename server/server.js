62adba438939ddc5e169f6cd0586e1b250d388b6
Deepak-132006 <deepakn132006@gmail.com>
2026-07-30 20:24:10 +0530

    fix: CORS handled

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-    origin: "https://mern-taskit.vercel.app/",
+    origin: "https://mern-taskit.vercel.app",
     credentials: true
 }
 ));
